d5d58abfc27933d845ee9255d5294bada23690b8
Rayer Tung <rayer@vista.aero>
2020-06-11 16:05:36 +0800

    Add Handle Sentences with keys

--- MonpaWrapper.py
+++ MonpaWrapper.py
@@ -8,5 +8,11 @@ class MonpaWrapper:
     def handle_sentences(self, sentences):
         ret = []
         for s in sentences:
-            ret += monpa.cut(s)
+            ret.append(monpa.cut(s))
+        return ret
+
+    def handle_sentences_with_keys(self, payload):
+        ret = []
+        for rec in payload:
+            ret.append({'key': rec['key'], 'result': monpa.cut(rec['sentence'])})
         return ret
--- server.py
+++ server.py
@@ -11,12 +11,19 @@ def parse():
     if request.method == 'GET':
         return jsonify({'response': 'ok!(GET)'})
     payload = request.json
-    if 'sentences' not in payload and 'sentences_with_keys':
+    if 'sentences' not in payload and 'sentence_with_keys' not in payload:
         return jsonify({'response': 'No sentences nor sentence_with_keys attribute are found!'})
 
-    ws_list = monpa.handle_sentences(payload['sentences'])
+    ws_list_1 = None
+    ws_list_2 = None
+    if 'sentences' in payload:
+        ws_list_1 = monpa.handle_sentences(payload['sentences'])
 
-    return jsonify({'response': ws_list})
+    if 'sentence_with_keys' in payload:
+        ws_list_2 = monpa.handle_sentences_with_keys(payload['sentence_with_keys'])
+
+    # Sentences with keys have higher property
+    return jsonify({'response': ws_list_2 if ws_list_1 is None else ws_list_1})
 
 @app.route('/health')
 def healthCheck():
